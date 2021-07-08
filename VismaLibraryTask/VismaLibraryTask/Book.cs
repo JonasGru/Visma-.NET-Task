afc736f035f6168007155e8c61e1d2c171f9923a
JonasGru <armed.sea.hunter@gmail.com>
2021-07-08 18:21:07 +0300

    book class, json start

---
+++ VismaLibraryTask/VismaLibraryTask/Book.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace VismaLibraryTask
+{
+    class Book
+    {
+        public string name;
+        public string author;
+        public string category;
+        public string language;
+        public DateTime publicationDate;
+        public string ISBN;
+        
+
+
+
+    }
+}
---
+++ VismaLibraryTask/VismaLibraryTask/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.IO;
+using System.Text.Json;
+using System.Text.Json.Serialization;
+
+namespace VismaLibraryTask
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+
+            string fileName = "../../../bookFile.json";
+            string jsonString = File.ReadAllText(fileName);
+            Console.WriteLine(jsonString);
+            Book book = JsonSerializer.Deserialize<Book>(jsonString);
+            Console.WriteLine($"Name: {book.name}");
+            Console.WriteLine($"ISBN: {book.ISBN}");
+            Console.ReadKey();
+        }
+    }
+}
