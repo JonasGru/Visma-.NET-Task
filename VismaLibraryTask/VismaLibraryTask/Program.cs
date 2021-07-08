f64ba4a0a478d75dcbde68817a941002f70a5b87
JonasGru <armed.sea.hunter@gmail.com>
2021-07-08 23:21:11 +0300

    minor changes

--- VismaLibraryTask/VismaLibraryTask/Program.cs
+++ VismaLibraryTask/VismaLibraryTask/Program.cs
@@ -9,24 +9,25 @@ namespace VismaLibraryTask
 {
     class Program
     {
+        static string fileName = "../../../bookFile.json";
+        static List<Book> booksInLibrary;
         static void Main()
         {
             Console.WriteLine("Hello World!");
 
-            string fileName = "../../../bookFile.json";
+           // Program p = new();
 
-            
 
-            string jsonString = File.ReadAllText(fileName);
-            Console.WriteLine(jsonString);
+
+            //string jsonString = File.ReadAllText(fileName);
+            //Console.WriteLine(jsonString);
+            ReadFromFile();
+            PrintLibrary();
+
             //Book book = JsonSerializer.Deserialize<Book>(jsonString);
             //List<Book> booksInLibrary = JsonSerializer.Deserialize<List<Book>>(jsonString);
-            List<Book> booksInLibrary =  JsonConvert.DeserializeObject<List<Book>>(jsonString);
-            foreach (Book b in booksInLibrary)
-            {
-                Console.WriteLine($"Name: {b.Name}");
-                Console.WriteLine($"ISBN: {b.ISBN}");
-            }
+            
+            
             
 
             //Book testBook = new Book("Test", "author", "testing", "English", DateTime.Parse("2000-01-01"), "0-000-000-000-001");
@@ -36,6 +37,21 @@ namespace VismaLibraryTask
             //Console.WriteLine(jsonString);
 
             Console.ReadKey();
+        }
+
+        static void ReadFromFile()
+        {
+            string jsonString = File.ReadAllText(fileName);
+            booksInLibrary = JsonConvert.DeserializeObject<List<Book>>(jsonString);
+        }
+
+        public static void PrintLibrary()
+        {
+            Console.WriteLine($"{"Name", 15} | {"Author", 15} | {"Category",15} | {"Language",15} | {"PublicationDate",15} | {"ISBN",20} ");
+            foreach (Book b in booksInLibrary)
+            {
+                Console.WriteLine($"{b.Name, 15} | {b.Author, 15} | {b.Category, 15} | {b.Language,15} | {b.PublicationDate.Year,15} | {b.ISBN,20}");
+            }
         }
     }
 }
