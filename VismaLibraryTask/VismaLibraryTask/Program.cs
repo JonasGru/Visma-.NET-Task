c3d15980efd85479e6aafc2f9c15dba296f58531
JonasGru <armed.sea.hunter@gmail.com>
2021-07-09 14:04:10 +0300

    Borrowing books

--- VismaLibraryTask/VismaLibraryTask/Program.cs
+++ VismaLibraryTask/VismaLibraryTask/Program.cs
@@ -43,7 +43,7 @@ namespace VismaLibraryTask
 
         public static void MainMenu()
         {
-            Console.WriteLine($"{"1 - Library contents", 15} | {"2 - Insert Book",15} | {"1 - Library contents",15} | {"9 - Save Library",15} | {"0 - Exit Library",15} |");
+            Console.WriteLine($"{"1 - Library contents", -15} | {"2 - Insert Book",-15} | {"3 - TakeBook",-15} | {"9 - Save Library",-15} | {"0 - Exit Library",-15} |");
             string input = Console.ReadLine();
             switch (input)
             {
@@ -54,7 +54,7 @@ namespace VismaLibraryTask
                     InsertBook();
                     break;
                 case "3":
-                    PrintLibrary();
+                    TakeBook();
                     break;
                 case "9":
                     SaveLibrary();
@@ -78,46 +78,49 @@ namespace VismaLibraryTask
 
         public static void PrintLibrary()
         {
-            Console.WriteLine($"{"Name", 15} | {"Author", 15} | {"Category",15} | {"Language",10} | {"P_Date",10} | {"ISBN",17} ");
+            Console.WriteLine($"{"NR", 4} | {"Name", -15} | {"Author", -15} | {"Category",-15} | {"Language",-10} | {"P_Date",-10} | {"ISBN",-17} ");
+            int nr = 0; //faster than booksInLibrary.IndexOf(b)?
             foreach (Book b in booksInLibrary)
             {
-                Console.WriteLine($"{b.Name, 15} | {b.Author, 15} | {b.Category, 15} | {b.Language,10} | {b.PublicationDate.Year,10} | {b.ISBN,17}");
+                Console.WriteLine($"{nr++,4} | {b.Name, -15} | {b.Author, -15} | {b.Category, -15} | {b.Language,-10} | {b.PublicationDate.Year,-10} | {b.ISBN,-17}");
             }
         }
 
         public static void InsertBook()
         {
             Console.WriteLine("write a book to insert:");
-            Console.WriteLine($"{"Name (string)",15} | {"Author (string)",15} | {"Category (string)",15} | {"Language (string)",10} | {"P_Date (DateTime)",10} | {"ISBN (string)",17} ");
-
-            Console.WriteLine("Name (string):");
-            string name = Console.ReadLine();
-
-            Console.WriteLine("Author (string):");
-            string author = Console.ReadLine();
-
-            Console.WriteLine("Category (string):");
-            string category = Console.ReadLine();
-
-            Console.WriteLine("Language (string):");
-            string language = Console.ReadLine();
-
-            Console.WriteLine("Publication Date (YYYY MM DD):");
-            DateTime p_date = DateTime.Parse(Console.ReadLine());
-
-            Console.WriteLine("ISBN (string):");
-            string isbn = Console.ReadLine();
-
+            //Console.WriteLine($"{"Name (string)",15} | {"Author (string)",15} | {"Category (string)",15} | {"Language (string)",10} | {"P_Date (DateTime)",10} | {"ISBN (string)",17} ");
             try
             {
+                Console.WriteLine("Name (string):");
+                string name = Console.ReadLine();
+
+                Console.WriteLine("Author (string):");
+                string author = Console.ReadLine();
+
+                Console.WriteLine("Category (string):");
+                string category = Console.ReadLine();
+
+                Console.WriteLine("Language (string):");
+                string language = Console.ReadLine();
+
+                Console.WriteLine("Publication Date (YYYY MM DD):");
+                DateTime p_date = DateTime.Parse(Console.ReadLine());
+
+                Console.WriteLine("ISBN (string):");
+                string isbn = Console.ReadLine();
+
                 Book instertBook = new(name, author, category, language, p_date, isbn);
                 AddBookToLibrary(instertBook);
             }
             catch (Exception)
             {
-
+                Console.WriteLine("Field inserted with a wrong value, try again");
+                InsertBook();
                 throw;
             }
+
+            
         }
 
         public static void AddBookToLibrary(Book bookToInsert)
@@ -127,7 +130,69 @@ namespace VismaLibraryTask
 
         public static void TakeBook()
         {
+            Console.WriteLine("select a book to take (write it's NR to console):");
+            int nr;
+            try
+            {
+                nr = int.Parse(Console.ReadLine());
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("please write an integer (number)");
+                //TakeBook();
+                throw;
+            }
+            Console.WriteLine("who will be taking this book?:");
+            string takenBy = Console.ReadLine();
 
+            Console.WriteLine("how long will you be borrowing this book? (MM DD, max 2 months):");
+            string borrowingLengh = Console.ReadLine();
+            DateTime date;// = DateTime.Parse(borrowingLengh);
+            try
+            {
+                date = DateTime.Parse(borrowingLengh);
+            }
+            catch (Exception)
+            {
+                Console.WriteLine("input is in wrong format");
+                throw;
+            }
+            TakeBookFromLibrary(nr, takenBy, date);
+        }
+
+        public static void TakeBookFromLibrary(int nr, string personBorrowing, DateTime date)
+        {
+            List<Book> results = booksInLibrary.FindAll(delegate (Book b) { return b.TakenBy == personBorrowing; });
+            Console.WriteLine("person borrowed: " + results.Count);
+            if(results.Count > 2)
+            {
+                Console.WriteLine($"person already borrowed {results.Count} books, we only allow borrowing 3 books!");
+                return;
+            }
+            if (date.CompareTo(DateTime.Parse("02 01")) < 1)
+            {
+                try
+                {
+                    booksInLibrary[nr].Taken = true;
+                    booksInLibrary[nr].TakenBy = personBorrowing;
+                    booksInLibrary[nr].TakenUntil = DateTime.Now.AddMonths(date.Month).AddDays(date.Day);
+                }
+                catch (IndexOutOfRangeException)
+                {
+                    Console.WriteLine("book not found");
+                    throw;
+                }
+                catch (Exception)
+                {
+
+                    throw;
+                }
+               
+            }
+            else
+            {
+                Console.WriteLine("borrowing term is too long, we only borrow books for 2 months");
+            }
         }
 
         public static void ReturnBook()
